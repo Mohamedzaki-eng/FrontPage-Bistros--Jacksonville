8de9423e58b84c7c7d4b2841ac429c37fd75e926
Mohamed Zaki <52196443+Mohamedzaki-eng@users.noreply.github.com>
2019-07-22 17:16:09 -0400

    First Commit

---
+++ bistros/index.php
@@ -0,0 +1,131 @@
+
+<!DOCTYPE html>
+<html>
+
+<head>
+  
+  <title>Bistros | Jacksonville</title>
+
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <!-- FontAwesome -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css">
+
+</head>
+    <body style="background-image: url(imgs1/res1.jpg);background-repeat: repeat;background-attachment:fixed;">
+      
+    <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top container">
+      <div class="container">
+            <a class="navbar-brand" href="#">BISTROS <span>Jacksonville, FL</span></a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar"> </button>
+         <div class="collapse navbar-collapse" id="collapsibleNavbar">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Reservations</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Jobs</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Contact Us</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#" class="lead"><strong>1-800-123-4567</strong></a>
+              </li>
+            </ul>
+         </div>
+      </div>
+    </nav>
+
+    <!-- Page Content -->    
+    <div class="row" style="margin-right: 10px;">
+      <div class="col-lg-12 text-center">
+          <h1 class="mt-5" style="padding-top: 70px; color: white;">Voted Best New Restaurant in Jacksonville!</h1> <br><br>
+          <p><button type="button" class="btn btn-warning">Order Online</button></p> <br><br><br>
+      </div>
+    </div>
+    <div class="container" style="background-color:#EEE8AA;opacity: .9;">     
+      <div class="row">
+         <div class="col-lg-12 text-center"><br>
+           <h3>Our Menus</h3><br>
+         </div> <br><br>
+    <div class="col-lg-4 text-center">
+        <img src="imgs1/breakfast.jpg" style = "border:2px solid brown; height: 200px; width: 300px; opacity: .9;border-radius:10%;"alt="BreakFast"><br><br><br><br>
+        <a href="#" class="btn btn-warning">Breakfast</a><br><br>
+    </div>
+    <div class="col-lg-4 text-center">
+        <img src="imgs1/lunch.jpg" style = "border:2px solid brown; height: 200px; width: 300px; opacity: .9;border-radius:10%;"alt="BreakFast"><br><br>
+        <br><br><a href="#" class="btn btn-warning">Lunch</a><br><br>
+    </div>
+    <div class="col-lg-4 text-center">
+        <img src="imgs1/dinner.jpg" style = "border:2px solid brown; height: 200px; width: 300px; opacity: .9;border-radius:10%;"alt="BreakFast"><br><br>
+        <br><br><a href="#" class="btn btn-warning">Dinner</a><br><br>
+    </div><br><br>
+      </div><br><br> <!--/row-->
+    </div> <!--/container-->
+        
+    <div class="container" style="background-color:#ffffff; opacity: .95;">    
+       <div class="row">
+          <div class="col-lg-6"><br>
+            <h3>Our Chef: Mark Ducasee</h3><br>
+               <p class="lead" style=""><img src="imgs1/chef.jpg" class="img-thumbnail" style="padding:1px;width:200px;float:right;"> <strong> Chef Mark Ducasee </strong>has 25 years of food service experience equips him to lead our team of culinary experts at <strong>Bistros</strong>.
+                Mark joined our team in 2014. He has experience as an Executive Chef at various resorts, hotels, and  private clubs. He oversees all aspects of food creation at the Club including all dinner, banquets,  buffets,  and a la carte menus. "In my kitchen, I strive to purchase the freshest ingredients, making frequent trips to Farmer's Market for  produce, the quality food is at the heart of all great cooking" <strong>Chef Mark</strong>. <br><br>   
+                <br><img src="imgs1/socialicons.jpg"alt="social_icons"></p>
+          </div>  
+       <div class="col-lg-6" style="background-color:lightgray;border-left:1px dashed gray;"><br>
+            <h3>Reviews</h3><br>
+              <p class="lead" style="text-align: justify;">
+              <i class="fas fa-star" style="color:gold;text-shadow: 1px 1px black;font-size:18px;"></i>
+              <i class="fas fa-star" style="color:gold;text-shadow: 1px 1px black;font-size:18px;"></i>
+              <i class="fas fa-star" style="color:gold;text-shadow: 1px 1px black;font-size:18px;"></i>
+              <i class="fas fa-star" style="color:gold;text-shadow: 1px 1px black;font-size:18px;"></i>
+              <i class="fas fa-star" style="color:gold;text-shadow: 1px 1px black;font-size:18px;"></i> 
+              "Bistros is my favorite restaurant. Last night, we had dinner there. We are always impressed with the great food and services."<br>
+              <span style="color:#30a64a;font-size:14px;float:right;">--Cristie Renne</span></p><br><br>
+
+              <p class="lead" style="text-align: justify;">
+              <i class="fas fa-star" style="color:gold;text-shadow: 1px 1px black;font-size:18px;"></i>
+              <i class="fas fa-star" style="color:gold;text-shadow: 1px 1px black;font-size:18px;"></i>
+              <i class="fas fa-star" style="color:gold;text-shadow: 1px 1px black;font-size:18px;"></i>
+              <i class="fas fa-star" style="color:gold;text-shadow: 1px 1px black;font-size:18px;"></i>
+              <i class="fas fa-star" style="color:gold;text-shadow: 1px 1px black;font-size:18px;"></i> 
+              "We can not say enough great things about the food and service!!! If we could give more stars we would!!!." <br>
+              <span style="color:#30a64a;font-size:14px;float:right;">--TMH Corporation</span></p><br><br>
+
+              <p class="lead" style="text-align: justify;">
+              <i class="fas fa-star" style="color:gold;text-shadow: 1px 1px black;font-size:18px;"></i>
+              <i class="fas fa-star" style="color:gold;text-shadow: 1px 1px black;font-size:18px;"></i>
+              <i class="fas fa-star" style="color:gold;text-shadow: 1px 1px black;font-size:18px;"></i>
+              <i class="fas fa-star" style="color:gold;text-shadow: 1px 1px black;font-size:18px;"></i>
+              <i class="fas fa-star" style="color:gold;text-shadow: 1px 1px black;font-size:18px;"></i>
+               "Now this place is 1st class dinning. 1st class everything. Not sure it gets to much better honestly." <br>
+               <span style="color:#30a64a;font-size:14px;float:right;">--Jack Moriens</span></p><br><br>
+
+              <p class="lead" style="text-align: justify;">
+              <i class="fas fa-star" style="color:gold;text-shadow: 1px 1px black;font-size:18px;"></i>
+              <i class="fas fa-star" style="color:gold;text-shadow: 1px 1px black;font-size:18px;"></i>
+              <i class="fas fa-star" style="color:gold;text-shadow: 1px 1px black;font-size:18px;"></i>
+              <i class="fas fa-star" style="color:gold;text-shadow: 1px 1px black;font-size:18px;"></i>
+              <i class="fas fa-star" style="color:gold;text-shadow: 1px 1px black;font-size:18px;"></i> 
+              "Finally a good steak. Service was perfect on every occasion I’ve been there. Steaks were cooked perfectly to the temperature asked for. If you like lobster, try the South African fried lobster tails as an appetizer. Sides were amazing, deserts were perfect. Thanks Cristie for the great service." <br><span style="color:#30a64a;font-size:14px;float:right;">--Chase Family</span></p>
+        </div> <!--/col-->
+      </div> <!--/row-->
+    </div> <!--/container-->
+        
+    <div class="container" style="background-color:#EEE8AA;color:gray;opacity: 1.0;">  
+      <div class="row">
+         <div class="col-lg-4">
+            <p class="text-left" style="line-height:2.9em;color:#000000;padding-top:14px;font-size:14px;">&copy; Copyright 2019, Bistros LLC</p>
+         </div>
+         <div class="col-lg-4">
+            <p class="text-lg-center" style="line-height:2.9em;color:#000000;padding-top:14px;font-size:14px;">Bistros, 2626 East Park Ave St., Jax, FL 32309</p>
+         </div>
+         <div class="col-lg-4">
+            <p class="text-lg-right" style="line-height:;padding-top:14px;font-size:14px;"><a href="#">Privacy Policy</a> | <a href="#">Terms of Use</a></p>
+         </div>
+      </div> <!--/row-->
+    </div> <!--/container-->
+
+</body>
+</html>
